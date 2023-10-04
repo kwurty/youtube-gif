962ada903af7e98bb8fb3e365444f9b59efb12cf
kwurty <16153951+kwurty@users.noreply.github.com>
2023-10-03 21:14:01 -0500

    first commit

---
+++ conversion.py
@@ -0,0 +1,34 @@
+# Filename: conversion.py
+from pytube import YouTube
+from moviepy.editor import *
+
+class Conversions:
+
+    def __init__(self, video: YouTube, export: str, startTime: int, clipLength: int):
+      self.video = video
+      self.export = export
+      self.startTime = startTime
+      self.clipLength = clipLength
+      self.videoLocation = None
+      self.youtube720pStream = None
+      self.youtube1080pStream = None
+      self.moviePyFile = None
+
+    def find720p(self):
+        self.youtube720pStream = self.video.streams.filter(file_extension="mp4").filter(resolution="720p").first()
+
+    def find1080p(self):
+        self.youtube1080pStream = self.video.streams.filter(file_extension="mp4").filter(resolution="1080p").first()
+
+    def downloadVideo(self):
+        self.videoLocation = self.youtube720pStream.download();
+
+    def setExport(self):
+        pass
+
+    def setClipLocation(self):
+        self.moviePyFile = VideoFileClip(self.videoLocation, audio=False)
+
+    def exportGif(self):
+        self.clip = self.moviePyFile.subclip(self.startTime, self.startTime + self.clipLength).resize(0.3)
+        self.clip.write_gif("E:\Programming\youtube-gif\output.gif", program='ffmpeg')
---
+++ program.py
@@ -0,0 +1,56 @@
+from pytube import YouTube
+from moviepy.editor import *
+from conversion import Conversions
+
+def main():
+
+    url = ""
+    time = ""
+    length = ""
+
+    while url=="":
+        print("Enter your youtube link: ")
+        url = input()
+
+    youtube = YouTube(url)
+
+    while time=="":
+        print("Enter your start time (HH:MM:SS): ")
+        time = input()
+        time = time.split(":")
+        if len(time) != 3:
+            print("Please enter a correct time format (HH:MM:SS)")
+            time = ""
+        
+        hours = time[0]
+        minutes = time[1]
+        seconds = time[2]
+        startTime = (int(hours) * 60 * 60) + (int(minutes) * 60) + int(seconds)
+
+        if(startTime > youtube.length):
+            print(f"The time entered is longer than the actual video ")
+            time=""
+
+    while length=="":
+        print("Enter your length in seconds (1-5): ")
+        length = int(input())
+        if(length > 5 or length < 1):
+            print("Time must be between 1-5 seconds")
+            length = ""
+
+    conversion = Conversions(youtube, "/", startTime, length)
+    conversion.find720p()
+    conversion.downloadVideo()
+    conversion.setClipLocation()
+    conversion.exportGif()
+
+    
+
+    # local = VideoFileClip(downloaded)
+
+    # clip = local.subclip(startTime, length)
+
+    # clip.write_gif("output.gif", fps=25)
+
+if __name__ == "__main__":
+    main()
